c8fe98adbd8f7a5944e4ed8ebbf2dda45a90be5f
David Robertson <david.m.robertson1@gmail.com>
2016-03-27 18:48:17 +0100

    Initial commit. Fleshing out the plan

---
+++ conundrum.js
@@ -0,0 +1,93 @@
+"use strict";
+var conundrum = {
+	word: "",
+	words: [],
+	nineLetterWords: [],
+	tiles: [],
+	history: null,
+	
+	newGame: null,
+	newWord: null,
+	shuffle: null
+};
+
+String.prototype.shuffle = function () {
+	var chars = this.split("");
+	for (var i = chars.length - 1; i > 0; i--) {
+		var j = randomInt(0, i);
+		var tmp = chars[i];
+		chars[i] = chars[j];
+		chars[j] = tmp;
+	}
+	return chars.join("");
+}
+
+// Returns a random integer between min (included) and max (excluded)
+function randomInt(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+conundrum.newGame = function () {
+	var index = randomInt(0, conundrum.nineLetterWords.length);
+	conundrum.word = conundrum.nineLetterWords[index].shuffle();
+	conundrum.newWord();
+	conundrum.body.classList.remove("loading");
+}
+
+conundrum.newWord = function () {
+	conundrum.history.innerHTML = "";
+	for (var i = 0; i < 9; i++){
+		conundrum.tiles[i].textContent = conundrum.word[i];
+	}
+}
+
+conundrum.shuffle = function () {
+	conundrum.word = conundrum.word.shuffle();
+	conundrum.newWord();
+}
+
+// main function
+
+function main() {
+	setupDOM();
+	getWordList();
+}
+
+function setupDOM () {
+	conundrum.body = document.getElementsByTagName("body")[0];
+	conundrum.tiles = document.getElementsByClassName('letter');
+	conundrum.history = document.getElementById('history');
+	document.getElementById("shuffle").addEventListener("click", conundrum.shuffle)
+	document.getElementById("reset").addEventListener("click", conundrum.newGame)
+}
+
+function getWordList () {
+	var words = localStorage.getItem("words")
+	if ( words === null ) {
+		var request = new XMLHttpRequest();
+		request.open("GET", "words.json");
+		request.addEventListener("load", gotWordList);
+		request.send();
+	} else {
+		parseWords(words);
+		conundrum.newGame();
+	}
+	
+}
+
+function gotWordList() {
+	localStorage.setItem("words", string);
+	parseWords(this.responseText);
+	conundrum.newGame();
+}
+
+function parseWords(string){
+	conundrum.words = JSON.parse(string).words;
+	for (var i = 0; i < conundrum.words.length; i++ ){
+		if (conundrum.words[i].length === 9) {
+			conundrum.nineLetterWords.push(conundrum.words[i])
+		}
+	}
+}
+
+document.addEventListener("DOMContentLoaded", main);
---
+++ words.py
@@ -0,0 +1,21 @@
+import json
+import sys
+
+def passes_filter(word):
+	#only accept characters A....Z
+	return all(65 <= ord(char) <= 90 for char in word)
+
+def main(words_file):
+	with open(words_file, 'rt', encoding='utf-8') as f:
+		words = list(f)
+	words = [w.strip().upper() for w in words]
+	words = [w for w in words if passes_filter(w)]
+	with open('words.json', 'wt', encoding='utf-8') as f:
+		json.dump({ "words": words }, f)
+
+if __name__ == "__main__":
+	try:
+		words_file = sys.argv[1]
+	except IndexError:
+		words_file = "/usr/share/dict/words"
+	main(words_file)
